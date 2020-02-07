e7ecc11e90e3eceb4abdbf0e1fd57a79510c9661
BRIGHTZNVICTORY <17602958129@163.com>
2020-02-07 20:48:50 +0800

    add liveplay subpage

--- pages/cus_pages/livePlayer/livePlayer.vue
+++ pages/cus_pages/livePlayer/livePlayer.vue
@@ -1,26 +1,23 @@
 <template>
-	<view>
-		<view>播放器</view>
-	</view>
+	<view></view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				player: null,    // 放置播放器
+				player: null,        // 放置播放器
 				liveplayurl: 'rtmp://zhangly.xyz/live/test?txSecret=67f1e2c359f98e8231b1c96913903f7a&txTime=5E04CAEF',  // 拉流地址
-				currentWebView: null ,//容器存放当前页面
-				
+				currentWebView: null //容器存放当前页面		
 			}
 		},
-		// onShow() {
-		// 	// 应用生命周期， 当uniapp 启动时，或从后台进入前台时调用	
-		// 	this.getwebView()
-		// },
 		onLoad() {
 			// 页面生命周期，页面加载时启动，参数是上个页面传递的参数，用于页面传参
-			this.getwebView()  //获取当前webviewå
+			this.getSubNvue()
+			this.getwebView()        //获取当前webviewå
+		},
+		onBackPress() {
+			this.player.close()
 		},
 		methods: {
 			/**
@@ -31,30 +28,42 @@
 				const page = pages[pages.length - 1]  // 获取当前页面
 				//#ifdef APP-PLUS
 				const webView = page.$getAppWebview()  // 得到当前页面的对象实例
-				// console.log(webView)
 				this.currentWebView = webView
-				this.plusPlay() //创建Liveplay 对象
+				// this.plusPlay() //创建Liveplay 对象
 				//#endif
                 this.plusPlay() //创建Liveplay 对象
 			},
 			/**
-			 * 创建播放器，即 拉流对象
+			 * 创建播放器，即拉流对象
 			 */
 			plusPlay() {
 				this.player = new plus.video.VideoPlayer('play', {
-					src: this.liveplayurl,
-					top: '100upx',
-					left: '0px',
-					width: '100%',
-					height: uni.getSystemInfoSync().windowHeight -100 + 'px',
-					position: 'static',
-					objectFit: 'fill'
-					
+					'src': this.liveplayurl,                                 // 视频地址
+					'top': '100upx',                                         // 播放器距屏幕上方的像素
+					'width': uni.getSystemInfoSync().windowWidth + 'px',     // 播放器的宽度
+					'height': uni.getSystemInfoSync().windowHeight + 'px',   // 播放器的高度
+					'position': 'static',                                    // 播放器的布局模式
+					'objectFit': 'fill',                                     // 视频的表现形式
+					'show-fullscreen-btn': false,                            // 播放器的是否显示全屏按钮
+					'show-play-btn': false,                                  // 播放器底部控制按钮
+					'autoplay': true,                                        // 视频是否自动播放
+					'direction': 0,                                          // 设置全屏时的视频方向
+					'poster': '',                                            // 视频封面图片地址
 				})
-				// console.log('player', this.player)
-				// console.log('webview', this.currentWebView)
 				this.currentWebView.append(this.player)
-				// console.log(this.currentWebView)
+			},
+			/**
+			 * 获取子窗口
+			 */
+			getSubNvue() {
+				const product = uni.getSubNVueById('product')
+				product.show()
+				const barrage = uni.getSubNVueById('barrage')
+				barrage.show()
+				const liveFooter = uni.getSubNVueById('liveFooter')
+				liveFooter.show()
+				const liveHeader = uni.getSubNVueById('liveHeader')
+				liveHeader.show()
 			}
 		}
 	}
